dae3f7f2720bba43ca311b0751e1113bae2d350b
Andreasgura <andre-56@hotmail.it>
2024-04-10 17:41:58 +0200

    heder and main

--- src/assets/components/MainComponent.vue
+++ src/assets/components/MainComponent.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
-        Main
-    </div>
+    <main>
+        <div class="container">
+            - ->Content goes here<- -
+        </div>
+    </main>
 </template>
 
 <script>
@@ -11,5 +13,10 @@
 </script>
 
 <style lang="scss" scoped>
-
+@use '../styles/partials/variables' as *;
+    main {
+        background-color: $mainbg;
+        color: white;
+        padding: 50px 0;
+    }
 </style>
