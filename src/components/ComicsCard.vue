42e6c52798f9394df4e235813d2d1dab449a0fa5
Andreasgura <andre-56@hotmail.it>
2024-04-11 17:53:05 +0200

    comics card

---
+++ src/components/ComicsCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col-12 col-md-6 col-lg-2 mb-3 ">
+        <div class="mb-3">
+            <img class="w-100"
+            :src="img" :alt="description">
+        </div>
+        <div class="text-uppercase ">
+            {{ description }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ComicsCard',
+        props: {
+            img: String,
+            description: String
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+img {
+    aspect-ratio: 1/1;
+    object-fit: cover;
+}
+</style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,22 +1,39 @@
 <template>
     <main>
         <div class="container">
-            - ->Content goes here<- -
+
+            <div class="row">
+                <ComicsCard v-for="(comic, index) in comics" :key="index" :img="comic.thumb"
+                    :description="comic.series" 
+                />
+            </div>
+
         </div>
     </main>
 </template>
 
 <script>
-    export default {
-        name: 'MainComponent',
+import ComicsCard from './ComicsCard.vue';
+import { comics } from '../data/store.js';
+export default {
+    name: 'MainComponent',
+    components: {
+        ComicsCard,
+    },
+    data() {
+        return {
+            comics,
+        }
     }
+}
 </script>
 
 <style lang="scss" scoped>
 @use '../assets/styles/partials/variables' as *;
-    main {
-        background-color: $mainbg;
-        color: white;
-        padding: 50px 0;
-    }
+
+main {
+    background-color: $mainbg;
+    color: white;
+    padding: 50px 0;
+}
 </style>
